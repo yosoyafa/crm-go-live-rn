0ef8a94780f9c60030b3ada2a4f366ad517e4c08
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2020-12-03 14:40:08 -0500

    Added Contex API to app

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
+import { Alert } from 'react-native';
 
 const Context = React.createContext();
 
@@ -13,101 +14,134 @@ export const Provider = ({ children }) => {
     const [beneficiarios, setBeneficiarios] = useState([]);
     const [numeroEmergencia, setNumeroEmergencia] = useState('018000915286');
 
-    /*const setUp = async (callback) => {
+    const [cartera, setCartera] = useState({});
+    const [user, setUser] = useState('');
+
+    const setUp = async () => {
         try {
-            const mUsuario = await AsyncStorage.getItem('usuario');
             const mLogged = await AsyncStorage.getItem('logged');
-            const mInvitado = await AsyncStorage.getItem('invitado');
-            if (mUsuario !== null) {
-                setUsuario(JSON.parse(mUsuario));
-            }
             if (mLogged !== null) {
-                setLogged(mLogged);
+                setLogged((mLogged));
             }
-            if (mInvitado !== null) {
-                setInvitado(mInvitado);
-            }
-            callback()
         } catch (e) {
-            console.log(e);
+            console.warn('error en setUp, logged', e);
+        }
+        try {
+            const mUser = await AsyncStorage.getItem('user');
+            if (mUser !== null) {
+                setUser(JSON.parse(mUser));
+            }
+        } catch (e) {
+            console.warn('error en setUp, user', e);
+        }
+        try {
+            const mCartera = await AsyncStorage.getItem('cartera');
+            if (mCartera !== null) {
+                setCartera(JSON.parse(mCartera));
+            }
+        } catch (e) {
+            console.warn('error en setUp, cartera', e);
         }
     }
 
-    const modifyUsuario = async (value) => {
+    const modifyUser = async value => {
         try {
             const jsonValue = JSON.stringify(value)
-            await AsyncStorage.setItem('usuario', jsonValue);
-            setUsuario(value)
+            await AsyncStorage.setItem('user', jsonValue);
+            setUser(value)
         } catch (e) {
-            // save error
+            console.warn('error en modifyUser', e);
         }
     }
 
-    const modifyLogged = async (value) => {
+    const modifyLogged = async value => {
         try {
             await AsyncStorage.setItem('logged', value);
             setLogged(value);
         } catch (e) {
-            // save error
+            console.warn('error en modifyLogged', e);
         }
     }
 
-    const modifyInvitado = async (value) => {
+    const modifyCartera = async cartera => {
         try {
-            await AsyncStorage.setItem('invitado', value);
-            setInvitado(value);
-        } catch (e) {
-            // save error
+            await AsyncStorage.setItem('cartera', JSON.stringify(cartera));
+            setCartera(cartera);
+        } catch (error) {
+            console.warn(`error modificando cartera: ${error}`)
         }
     }
 
-    const getPlanes = async (cc) => {
+    const login = async (user, md5, lat, lon, fecha) => {
         try {
-            let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getAsegurado.php?cedula=${cc}`);
-            let json = await response.json();
-            setPlanes(json);
+            let response = await fetch(`https://integracionip.com/app/getUser.php?user_name=${user}&latitud=${lat}&longitud=${lon}&fecha=${fecha}`);
+            let resp = await response.json();
+            if (resp.user_name !== 'Usuario NO Autorizado para APP' && md5 === resp.user_hash) {
+                modifyLogged('1');
+                modifyUser(resp);
+            } else {
+                if (resp.user_name === 'Usuario NO Autorizado para APP') {
+                    Alert.alert('Error', resp.user_name);
+                } else {
+                    Alert.alert('Error', 'Usuario o contraseña incorrecta');
+                }
+            }
         } catch (error) {
-            console.error(error);
+            console.error('error en login', error);
+            Alert.alert('Error', 'Revisa tu conexión a internet');
         }
     };
 
-    const getBeneficiarios = async (cc) => {
-        try {
-            let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getBeneficiarios.php?cedula=${cc}`);
-            let json = await response.json();
-            setBeneficiarios(json);
-        } catch (error) {
-            console.error(error);
+    const downloadCartera = async (id) => {
+        if (id) {
+            try {
+                let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getCartera1.php?user_id=${id}`);
+                console.log(response);
+                let json = await response.json();
+                const today = new Date();
+                const mCartera = {
+                    fecha: `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`,
+                    contratos: json
+                }
+                modifyCartera(mCartera);
+            } catch (error) {
+                console.error('error en downloadcartera', error);
+            }
         }
     };
 
-    const getObituario = async (cc) => {
+    const recaudo = async (lat, lon, cc, valor, fecha, observacion, fdp) => {
         try {
-            let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getAsegurado.php?cedula=${cc}`);
+            let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getRecaudos.php?user_name=${user.user_name}&latitud=${lat}&longitud=${lon}&numerodocumento=${cc}&valorrecaudo=${valor}&id=${user.id}&rc=${user.iniciales_numerador}&fecha_hora=${fecha}&detallerecaudo=${observacion}&forma_de_pago=${fdp}`);
             let json = await response.json();
-            //console.log(json);
-            setObituario(json.results);
+            console.log(json);
         } catch (error) {
-            console.error(error);
+            console.error('error en recaudo', error);
         }
-    };*/
+    }
+
+    const gestion = async (lat, lon, cc, tipoGestion, fecha, acuerdo, fechaAcuerdo, valorAcuerdo, descripcion, resultadoGestion) => {
+        try {
+            let response = await fetch(`https://ws.crmolivosvillavicencio.com/app/getGestionCartera.php?user_name=${user.user_name}&latitud=${lat}&longitud=${lon}&numerodocumento=${cc}&tipo_gestion=${tipoGestion}&fecha=${fecha}&rc=${user.iniciales_numerador}&fecha_hora=${fecha}&acuerdo_pago=${acuerdo}&fechaAcuerdo=${fechaAcuerdo}&valor_acuerdo=${valorAcuerdo}&descripcion=${descripcion}&resultado_gestion=${resultadoGestion}&user_id=${user.id}`);
+            let json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error('error en recaudo', error);
+        }
+    }
 
     return (
         <Context.Provider value={{
-            /*setUp,
-            modifyInvitado,
+            setUp,
+            downloadCartera,
+            login,
+            modifyCartera,
             modifyLogged,
-            modifyUsuario,
-            getObituario,
-            getPlanes,
-            getBeneficiarios,
-            usuario,
-            invitado,
+            modifyUser,
+            recaudo,
+            cartera,
             logged,
-            obituario,
-            planes,
-            beneficiarios,*/
-            numeroEmergencia
+            user,
         }}>
             {children}
         </Context.Provider>

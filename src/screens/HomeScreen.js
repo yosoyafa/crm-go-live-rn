5dcd396b9da64c8b955088e91ac650e70d0a5bb0
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2020-12-03 14:40:26 -0500

    Added Home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,88 +1,122 @@
-import React, { useState, useContext } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
+import React, { useState, useContext, useEffect } from 'react';
+import { Text, View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import { IconButton } from 'react-native-paper';
+import { IconButton, ActivityIndicator } from 'react-native-paper';
+
 import Context from '../context/Context';
 import ContratoCard from '../components/ContratoCard';
 
 const HomeScreen = ({ navigation }) => {
-    const [term, setTerm] = useState('');
-    //Geolocation.getCurrentPosition(info => console.log(info));
 
-    const contratos = [
-        {
-            contrato: '9991',
-            nombre: 'cliente 1',
-            cedula: '123',
-            valor: '12000'
-        },
-        {
-            contrato: '9992',
-            nombre: 'cliente 1',
-            cedula: '123',
-            valor: '12000'
-        },
-        {
-            contrato: '9993',
-            nombre: 'cliente 1',
-            cedula: '123',
-            valor: '12000'
-        },
-        {
-            contrato: '9994',
-            nombre: 'cliente 1',
-            cedula: '123',
-            valor: '12000'
-        },
-        {
-            contrato: '9995',
-            nombre: 'cliente 1',
-            cedula: '123',
-            valor: '12000'
-        },
-    ];
+    const { downloadCartera, cartera, user } = useContext(Context);
+
+    const [lista, setLista] = useState(cartera.contratos);
+    const [term, setTerm] = useState('');
+    const [closeVisible, setCloseVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (cartera) {
+            setLista(cartera.contratos);
+        }
+    }, [cartera]);
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <IconButton
-                    icon="menu"
-                    size={30}
-                    onPress={() => navigation.toggleDrawer()}
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <IconButton
+                        icon="menu"
+                        size={30}
+                        onPress={() => navigation.openDrawer()}
+                    />
+                    <View style={{ flexDirection: 'row' }}>
+                        <IconButton
+                            icon="cloud-download"
+                            size={30}
+                            onPress={async () => {
+                                setLoading(true);
+                                downloadCartera(user.id).then(
+                                    () => {
+                                        setLista(cartera.contratos);
+                                        setLoading(false);
+                                    }
+                                )
+                            }}
+                        />
+                        <IconButton
+                            icon="sync"
+                            size={30}
+                            onPress={() => console.log('Pressed')}
+                        />
+                    </View>
+                </View>
+                <SearchBar
+                    onChangedTerm={setTerm}
+                    onSubmitTerm={() => {
+                        if (term === '') {
+                            setLista(cartera.contratos);
+                        } else {
+                            setLista(cartera.contratos);
+                            setLista(lista.filter(contrato => contrato.cedula === term));
+                            setCloseVisible(true);
+                        }
+                    }}
                 />
-                <IconButton
-                    icon="sync"
-                    size={30}
-                    onPress={() => console.log('Pressed')}
-                />
+                <View style={{ flexDirection: 'row-reverse', marginTop: 10 }}>
+                    {closeVisible && <IconButton
+                        icon='close-circle'
+                        color='#96158C'
+                        size={20}
+                        onPress={() => {
+                            setLista(cartera.contratos);
+                            setCloseVisible(false);
+                            setTerm('');
+                        }}
+                    />}
+                </View>
+
+                {!!lista && !loading ?
+                    <>
+                        {lista.length !== 0 ?
+                            <>
+                                <Text style={{ textAlign: 'right' }}>{cartera.fecha}</Text>
+                                <FlatList
+                                    data={lista}
+                                    keyExtractor={(item) => item.numeropoliza}
+                                    renderItem={({ item, index, }) => {
+                                        let isLast;
+                                        lista.length === index + 1 ? isLast = true : isLast = false;
+                                        return <ContratoCard
+                                            item={item}
+                                            last={isLast}
+                                            gestion={() => navigation.navigate('Gestion', { contrato: item })}
+                                            recaudo={() => navigation.navigate('Recaudo', { contrato: item })}
+                                            edicion={() => navigation.navigate('Edicion', { contrato: item })}
+                                        />
+                                    }}
+                                />
+                            </>
+                            :
+                            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                                <Text style={{ fontSize: 14, color: 'gray' }} >No se han encontrado resultados para la búsqueda</Text>
+                            </View>
+                        }
+                    </>
+                    :
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                        <ActivityIndicator color='purple' size='large' />
+                    </View>
+                }
             </View>
-            <SearchBar
-                onChangedTerm={setTerm}
-                onSubmitTerm={() => {
-                    if (term) {
-                        //searchMovie(term)
-                    }
-                }}
-            />
-            <FlatList
-                data={contratos}
-                keyExtractor={(item) => item.contrato}
-                renderItem={({ item, index, }) => {
-                    if (contratos.length === index + 1) {
-                        return <ContratoCard item={item} last={true} />
-                    }
-                    return <ContratoCard item={item} last={false} />
-                }}
-            />
-        </View>
+        </SafeAreaView >
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 15
+        paddingHorizontal: 20
     },
     header: {
         flexDirection: 'row',

8cfef7d4b7d21dd39e57045be27e17c7017681c0
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2021-05-20 21:42:51 -0500

    Added print feature

--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -1,18 +1,32 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Text, View, FlatList, ActivityIndicator } from 'react-native';
+
 import HistoryCard from '../components/HistoryCard';
 import MenuBar from '../components/MenuBar';
 import Context from '../context/Context';
 import Tabs from '../components/Tabs';
 
+import { createTicketGestion, createTicketRecaudo, print } from '../utils/tools';
+
 const HistoryScreen = ({ navigation }) => {
 
-    const { history, getHistory, user } = useContext(Context);
+    const { history, getHistory, user, parametrosGestion, parametrosRecaudo } = useContext(Context);
 
     const [lista, setLista] = useState([]);
     const [refreshing, setRefreshing] = useState(false);
 
     const [active, setActive] = useState(0);
+
+    const [openCard, setOpenCardState] = useState('');
+
+    const setOpenCard = (id) => {
+        console.log(id);
+        if (openCard === id) {
+            setOpenCardState('');
+        } else {
+            setOpenCardState(id);
+        }
+    };
 
 
     useEffect(() => {
@@ -32,9 +46,16 @@ const HistoryScreen = ({ navigation }) => {
 
     const refresh = async () => {
         setRefreshing(true);
-        await getHistory(user.id).then(() => {
-            setRefreshing(false);
-        });
+        await getHistory(user.id);
+        switch (active) {
+            case 0:
+                setLista(history?.recaudos || []);
+                break;
+            case 1:
+                setLista(history?.gestiones || []);
+                break;
+        }
+        setRefreshing(false);
     };
 
     const menuBarItems = [
@@ -75,20 +96,23 @@ const HistoryScreen = ({ navigation }) => {
                     style={{ paddingHorizontal: 20, paddingTop: 10 }}
                     keyExtractor={(item) => item.id}
                     renderItem={({ item, index }) => {
-                        let isLast;
-                        lista.length === index + 1 ? isLast = true : isLast = false;
-                        return !isLast ?
-                            <HistoryCard
-                                type={active === 0 ? 'recaudo' : 'gestion'}
-                                recibo={item}
-                            />
-                            :
-                            <View style={{ marginBottom: 10 }}>
+                        const isLast = lista.length === index + 1;
+                        return (
+                            <View style={{ marginBottom: isLast ? 10 : 0 }}>
                                 <HistoryCard
                                     type={active === 0 ? 'recaudo' : 'gestion'}
                                     recibo={item}
+                                    optionsVisible={active === 0 ? item.numerorecibo === openCard : item.name === openCard}
+                                    setOptionsVisible={setOpenCard}
+                                    last={isLast}
+                                    print={active === 0 ?
+                                        () => print(createTicketRecaudo(item, item.numerorecibo, item.valorrecaudado, item.detalledelpago, parametrosRecaudo, true))
+                                        :
+                                        () => print(createTicketGestion(item, item.name, item.resultadogestion, parametrosGestion, true))
+                                    }
                                 />
                             </View>
+                        )
                     }}
                     ListEmptyComponent={<View style={{ flex: 1, alignItems: 'center', marginVertical: 50 }}>
                         <Text>No hay recibos disponibles</Text>

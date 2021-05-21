5c4590490601193e1d8236f8a8d76966c0df34e1
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2021-05-20 21:41:21 -0500

    Added tests and mocks

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,7 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', () => {
+  const app = renderer.create(<App />).toJSON();
+  expect(app).toMatchSnapshot();
 });
